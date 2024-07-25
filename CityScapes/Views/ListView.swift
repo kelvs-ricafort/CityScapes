9ba433f4df58bf020284800110306a17a0d29875
Kelvin Ricafort <kelvs.r@outlook.com>
2024-07-25 16:04:01 +0800

    - Enable dynamic images in ListView - Make image dynamic in business detail view

--- CityScapes/Views/ListView.swift
+++ CityScapes/Views/ListView.swift
@@ -15,8 +15,24 @@ struct ListView: View {
             ForEach(model.businesses) { b in
                 VStack(spacing: 20) {
                     HStack(spacing: 0) {
-                        Image("list-placeholder-image")
-                            .padding(.trailing, 16)
+                        if let imageUrl = b.imageUrl {
+                            // Display the business image
+                            AsyncImage(url: URL(string: imageUrl)!) { image in
+                                image
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                                    .padding(.trailing, 16)
+                            } placeholder: {
+                                ProgressView()
+                                    .frame(width: 50, height: 50)
+                            }
+
+                        } else {
+                            Image("list-placeholder-image")
+                                .padding(.trailing, 16)
+                        }
                         VStack(alignment: .leading) {
                             Text(b.name ?? "Location not found")
                                 .font(Font.system(size: 15))
